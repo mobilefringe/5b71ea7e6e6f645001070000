b2f192d28c73af73eb9f57e73b3df18123ace9e7
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-04-18 11:38:02 -0400

    Pleasant Hill 2.0 1.0  /main.js

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ require.config({
         'google-map-api': 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCukCjH3fsuDYBdI44hZKL43m60jEToJjY',
         'google_map': 'https://mmvue.codecloudapp.com/google_map.vue?noext',
         //bugsnag intergration depedencies
-        'bugsnag': 'https://mmvue.codecloudapp.com/bugsnag.vue?noext',
+        'bugsnag': 'https://mmvue.codecloudapp.com/bugsnag',
         'bugsnag-plugin-vue': 'https://mmvue.codecloudapp.com/google_map.vue?noext',
     }
 });
