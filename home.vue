5a3bf7ad32abadf0df6d64cc123a99d542f9e1f7
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-09-25 11:14:28 -0400

    Pleasant Hill 2.0 1.3  /home.vue

--- home.vue
+++ home.vue
@@ -66,7 +66,7 @@
                         </div>
                     </div>
                     <h2 v-if="instaFeed.length > 0" class="home_title center">In Our Feed</h2>
-                    <div class="row hidden-xs margin_60">
+                    <div v-if="instaFeed.length > 0" class="row hidden-xs margin_60">
                         <div class="col-md-8 col-md-offset-2">
                             <div class="insta-feed-container">
                                 <div class="insta-feed-image " v-for="(item, index) in instaFeed">
