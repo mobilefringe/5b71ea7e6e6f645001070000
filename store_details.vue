1626ee20df1b8fc0415425808246eb996bfb0b06
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-11-30 12:35:02 -0500

    Pleasant Hill 2.0 1.4  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -5,7 +5,7 @@
             <div v-if="dataLoaded" v-cloak>
                 <div class="inside_page_header" v-if="pageBanner" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
                     <div class="main_container position_relative">
-                        <h2>{{ currentStore.name }}</h2>
+                        <h1>{{ currentStore.name }}</h1>
                     </div>
                 </div>
                 <div class="main_container">
